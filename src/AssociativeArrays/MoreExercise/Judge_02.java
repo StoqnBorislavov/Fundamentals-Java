a5402ae02befa4d10a51e516df4f0aa97c6efb6c
Stoqn <sbstoyanov27@gmail.com>
2021-03-14 20:23:39 +0200

    Associative Arrays - More Exercise - Judge fixed

--- src/AssociativeArrays/MoreExercise/Judge_02.java
+++ src/AssociativeArrays/MoreExercise/Judge_02.java
@@ -2,13 +2,14 @@ package AssociativeArrays.MoreExercise;
 
 import java.util.*;
 import java.util.concurrent.Phaser;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Judge_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String input = sc.nextLine();
-        Map<String, List<Map<String, Integer>>> playersMap = new LinkedHashMap<>();
+        Map<String, Map<String, Integer>> playersMap = new LinkedHashMap<>();
         Map<String, Integer> contestantMap = new HashMap<>();
         while (!input.equals("no more time")) {
             String[] command = input.split(" -> ");
@@ -19,63 +20,47 @@ public class Judge_02 {
 
             input = sc.nextLine();
         }
-//        playersMap.entrySet().stream()
-//                .forEach(e ->{
-//                    System.out.printf("%s: %d participants%n", e.getKey(), e.getKey().length());
-//                    for (List<Map<String, Integer>> value : playersMap.values()) {
-//                        for (int i = 0; i < value.size(); i++) {
-//                            value.get(i).values().com
-//                        }
-//                    }
-//                });
-
-            //iterate over outer map
-        for(Map.Entry<String, List<Map<String, Integer>>> topEntry : playersMap.entrySet()) {
-            String topKey = topEntry.getKey();
+        //iterate over outer map
+        for (Map.Entry<String, Map<String, Integer>> entry : playersMap.entrySet()) {
+            String key = entry.getKey();
             //iterate over list
-            System.out.printf("%s: %d participants%n", topKey, topEntry.getValue().size());
-            for(Map<String, Integer> innerMap : topEntry.getValue()) {
-                //iterate over inner map
-                int counter = 1;
-                innerMap.entrySet().stream()
-                        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
-                        .thenComparing(Map.Entry.comparingByKey()))
-                        .forEach(e ->{
-                            System.out.printf("%d. %s <::> %d%n", counter, e.getKey(), e.getValue());
-                                });
-            }
+            System.out.printf("%s: %d participants%n", key, entry.getValue().size());
+            AtomicInteger count = new AtomicInteger(0);
+            entry.getValue().entrySet().stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
+                            .thenComparing(Map.Entry.comparingByKey()))
+                    .forEach(e -> {
+                        System.out.printf("%d. %s <::> %d%n", count.incrementAndGet(), e.getKey(), e.getValue());
+                    });
         }
+        System.out.println("Individual standings:");
+        AtomicInteger count = new AtomicInteger(0);
+        contestantMap.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .forEach(e -> System.out.printf
+                        ("%d. %s -> %d%n", count.incrementAndGet(), e.getKey(), e.getValue()));
+
     }
 
+
     private static void addContestant(String player, String contest, int points,
-                                      Map<String, List<Map<String, Integer>>> playersMap,
+                                      Map<String, Map<String, Integer>> playersMap,
                                       Map<String, Integer> contestantMap) {
-        if (!playersMap.containsKey(contest)) {
-            playersMap.put(contest, new ArrayList<>());
-            Map<String, Integer> currentContestant = new HashMap<>();
-            currentContestant.put(player, points);
-            playersMap.get(contest).add(currentContestant);
-            if(contestantMap.containsKey(player)) {
-                contestantMap.put(player, contestantMap.get(player) + points);
-            } else{
-                contestantMap.put(player, points);
-            }
-        } else if (playersMap.containsKey(contest)) {
-            playersMap.get(contest).stream()
-                    .forEach(e -> {
-                        if (e.containsKey(player)) {
-                            if (e.get(player) <= points) {
-                                e.put(player, points);
-                            }
-                        } else {
-                            e.put(player, points);
-                        }
-                    });
-            if(contestantMap.containsKey(player)) {
-                contestantMap.put(player, contestantMap.get(player) + points);
-            } else{
-                contestantMap.put(player, points);
-            }
+        if(!contestantMap.containsKey(player)){
+            contestantMap.put(player, points);
+
+        } else if(!playersMap.containsKey(contest) || !playersMap.get(contest).containsKey(player)){
+            contestantMap.put(player, contestantMap.get(player) + points);
+        } else if(contestantMap.get(player) < points){
+            contestantMap.put(player, points);
         }
+        if (!playersMap.containsKey(contest)) {
+            playersMap.put(contest, new HashMap<>());
+            playersMap.get(contest).put(player, points);
+        } else if (playersMap.containsKey(contest)) {
+            playersMap.get(contest).put(player, points);
+        }
+
     }
 }
