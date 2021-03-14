eb6ce66747209f1a13d44a8e7f3f291bb5367f8d
Stoqn <sbstoyanov27@gmail.com>
2021-03-14 21:21:40 +0200

    Text Processing - More Exercise - Winning Ticket fixed

--- src/TextProcessing/MoreExercise/WinningTicket_06.java
+++ src/TextProcessing/MoreExercise/WinningTicket_06.java
@@ -25,11 +25,19 @@ public class WinningTicket_06 {
             String reallySpecialCharRight = "";
             String leftSideTicket = ticket.substring(0, ticket.length() / 2);
             String rightSideTicket = ticket.substring(ticket.length() / 2);
-            for (int i = 0; i < leftSideTicket.length(); i++) {
+            for (int i = 0; i < leftSideTicket.length() - 1; i++) {
                 char currentChar = leftSideTicket.charAt(i);
 //                '@', '#', '$' and '^
                 if (currentChar == '@' || currentChar == '#'
                         || currentChar == '$' || currentChar == '^' && currentChar == leftSideTicket.charAt(i + 1)) {
+                    if (i + 1 == leftSideTicket.length() - 1 && ((leftSideTicket.charAt(i + 1) + "").equals(reallySpecialCharLeft))) {
+                        countLeft += 2;
+                        if (countLeft > maxCountLeft) {
+                            maxCountLeft = countLeft;
+                            reallySpecialCharLeft = specialCharLeft;
+                        }
+                        break;
+                    }
                     countLeft++;
                     specialCharLeft = currentChar + "";
                 } else {
@@ -43,28 +51,36 @@ public class WinningTicket_06 {
             }
             int countRight = 0;
             int maxCountRight = 0;
-            for (int i = 0; i < rightSideTicket.length(); i++) {
+            for (int i = 0; i < rightSideTicket.length() - 1; i++) {
                 char currentChar = rightSideTicket.charAt(i);
 //                '@', '#', '$' and '^
                 if (currentChar == '@' || currentChar == '#'
                         || currentChar == '$' || currentChar == '^' && currentChar == leftSideTicket.charAt(i + 1)) {
+                    if (i + 1 == rightSideTicket.length() - 1 && ((rightSideTicket.charAt(i + 1) + "").equals(reallySpecialCharRight))) {
+                        countRight += 2;
+                        if (countRight > maxCountRight) {
+                            maxCountRight = countRight;
+                            reallySpecialCharRight = specialCharRight;
+                        }
+                        break;
+                    }
                     countRight++;
-                    specialCharRight= currentChar + "";
+                    specialCharRight = currentChar + "";
                 } else {
                     countRight = 0;
                 }
                 if (countRight > maxCountRight) {
                     maxCountRight = countRight;
-                    reallySpecialCharRight= specialCharRight;
+                    reallySpecialCharRight = specialCharRight;
                 }
             }
             ticket = "\"" + ticket + "\"";
-            if (maxCountLeft == maxCountRight && maxCountLeft >= 6 && maxCountLeft < 10 && reallySpecialCharLeft.equals(reallySpecialCharRight)) {
+            if (maxCountRight == 10 && maxCountLeft == 10 && reallySpecialCharLeft.equals(reallySpecialCharRight)) {
 
-                System.out.printf("ticket %s - %d%s%n", ticket, maxCountLeft, reallySpecialCharLeft);
-            } else if (maxCountLeft == maxCountRight && maxCountLeft >= 10 && reallySpecialCharLeft.equals(reallySpecialCharRight)) {
+                System.out.printf("ticket %s - %d%s Jackpot!%n", ticket, Math.min(maxCountLeft, maxCountRight), reallySpecialCharLeft);
+            } else if (maxCountRight >= 6 && maxCountRight <= 10 && maxCountLeft >= 6 && maxCountLeft <= 10 && reallySpecialCharLeft.equals(reallySpecialCharRight)) {
 
-                System.out.printf("ticket %s - %d%s Jackpot!%n", ticket, maxCountLeft, reallySpecialCharLeft);
+                System.out.printf("ticket %s - %d%s%n", ticket, Math.min(maxCountLeft, maxCountRight), reallySpecialCharLeft);
             } else {
                 System.out.printf("ticket %s - no match%n", ticket);
             }
