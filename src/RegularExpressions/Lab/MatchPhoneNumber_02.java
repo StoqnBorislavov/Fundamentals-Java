095c75b25135696474f1bd0308ae7898112868b6
Stoqn <sbstoyanov27@gmail.com>
2021-03-17 21:38:45 +0200

    Regular Expressions - Lab - Added Match Full Name - Solution

---
+++ src/RegularExpressions/Lab/MatchPhoneNumber_02.java
@@ -0,0 +1,24 @@
+package RegularExpressions.Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String regex = "\\+359([- ])2\\1\\d{3}\\1\\d{4}\\b";
+        String phones = sc.nextLine();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phones);
+
+        List<String> matcherPhones = new ArrayList<>();
+        while(matcher.find()){
+            matcherPhones.add((matcher.group()));
+        }
+        System.out.println(matcherPhones.toString().replaceAll("[\\[\\]]", ""));
+
+    }
+}
