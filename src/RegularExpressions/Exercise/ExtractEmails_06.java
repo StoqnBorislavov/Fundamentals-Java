a8c7944e11020d906a7ff69c7abe846f24dab86f
Stoqn <sbstoyanov27@gmail.com>
2021-03-21 11:38:17 +0200

    Regular Expressions - Exercise - Added ExtractEmails_06 - Solution

---
+++ src/RegularExpressions/Exercise/ExtractEmails_06.java
@@ -0,0 +1,20 @@
+package RegularExpressions.Exercise;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractEmails_06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        String regex = "[\\w\\-.]+@([\\w-]+\\.)+[\\w-]+";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
