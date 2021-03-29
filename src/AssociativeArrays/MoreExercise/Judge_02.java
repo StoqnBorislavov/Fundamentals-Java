28b8c77949bce376117df481d7e0a6b4c839ff4e
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-03-30 00:09:05 +0300

    Associative Arrays - More Exercise - Judge - Fixed solution

--- src/AssociativeArrays/MoreExercise/Judge_02.java
+++ src/AssociativeArrays/MoreExercise/Judge_02.java
@@ -12,7 +12,7 @@ public class Judge_02 {
         Map<String, Map<String, Integer>> playersMap = new LinkedHashMap<>();
         Map<String, Integer> contestantMap = new HashMap<>();
         while (!input.equals("no more time")) {
-            String[] command = input.split(" -> ");
+            String[] command = input.split(" \\-> ");
             String player = command[0];
             String contest = command[1];
             int points = Integer.parseInt(command[2]);
@@ -27,8 +27,15 @@ public class Judge_02 {
             System.out.printf("%s: %d participants%n", key, entry.getValue().size());
             AtomicInteger count = new AtomicInteger(0);
             entry.getValue().entrySet().stream()
-                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
-                            .thenComparing(Map.Entry.comparingByKey()))
+//                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
+//                            .thenComparing(Map.Entry.comparingByKey()))
+                    .sorted((left, right) -> {
+                        int result = right.getValue().compareTo(left.getValue());
+                        if (result == 0) {
+                            result = left.getKey().compareTo(right.getKey());
+                        }
+                        return result;
+                    })
                     .forEach(e -> {
                         System.out.printf("%d. %s <::> %d%n", count.incrementAndGet(), e.getKey(), e.getValue());
                     });
@@ -36,8 +43,15 @@ public class Judge_02 {
         System.out.println("Individual standings:");
         AtomicInteger count = new AtomicInteger(0);
         contestantMap.entrySet().stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
-                        .thenComparing(Map.Entry.comparingByKey()))
+//                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
+//                        .thenComparing(Map.Entry.comparingByKey()))
+                .sorted((left, right) -> {
+                    int result = right.getValue().compareTo(left.getValue());
+                    if (result == 0) {
+                        result = left.getKey().compareTo(right.getKey());
+                    }
+                    return result;
+                })
                 .forEach(e -> System.out.printf
                         ("%d. %s -> %d%n", count.incrementAndGet(), e.getKey(), e.getValue()));
 
@@ -47,19 +61,23 @@ public class Judge_02 {
     private static void addContestant(String player, String contest, int points,
                                       Map<String, Map<String, Integer>> playersMap,
                                       Map<String, Integer> contestantMap) {
-        if(!contestantMap.containsKey(player)){
+        if (!contestantMap.containsKey(player)) {
             contestantMap.put(player, points);
 
-        } else if(!playersMap.containsKey(contest) || !playersMap.get(contest).containsKey(player)){
+        } else if (!playersMap.containsKey(contest) || !playersMap.get(contest).containsKey(player)) {
             contestantMap.put(player, contestantMap.get(player) + points);
-        } else if(contestantMap.get(player) < points){
+        } else if (playersMap.get(contest).containsKey(player) && contestantMap.get(player) < points) {
             contestantMap.put(player, points);
         }
         if (!playersMap.containsKey(contest)) {
             playersMap.put(contest, new HashMap<>());
             playersMap.get(contest).put(player, points);
-        } else if (playersMap.containsKey(contest)) {
+        } else if (playersMap.containsKey(contest) && !playersMap.get(contest).containsKey(player)) {
             playersMap.get(contest).put(player, points);
+        } else if (playersMap.containsKey(contest) && playersMap.get(contest).containsKey(player)) {
+            if (playersMap.get(contest).get(player) < points) {
+                playersMap.get(contest).put(player, points);
+            }
         }
 
     }
